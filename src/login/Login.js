c8f69ad256fe0171107fbbec19c450be396d8dba
Dor Levi <dorlev1204@gmail.com>
2022-04-25 13:41:38 +0300

    validate login and register

--- src/login/Login.js
+++ src/login/Login.js
@@ -5,6 +5,8 @@ import Register from './Register.js'
 
 function Login({ registered, setCurrentUser }) {
     let [register, setRegister] = useState(false);
+    let [notes, setNotes] = useState("");
+    let [userState, setUserState] = useState("");
 
     const handleKey = (e) => {
         if (e.key === "Enter") {
@@ -15,10 +17,28 @@ function Login({ registered, setCurrentUser }) {
     const handle = () => {
         var userName = document.getElementById("username").value;
         var password = document.getElementById("password").value;
-        if (contacts.findIndex((user) => { return user.name == userName && user.pass == password }) !== (-1)) {
-            registered(true);
-            setCurrentUser(userName);
+        if (validate(userName, password)) {
+            if (contacts.findIndex((user) => { return user.name == userName && user.pass == password }) !== (-1)) {
+                registered(true);
+                setCurrentUser(userName);
+            }
+            setUserState("User does not exist, please register");
         }
+    }
+
+    const validate = (userName, password) => {
+        let reg = /^[a-zA-Z0-9]+$/i;
+        let note = '';
+        let test = reg.test(userName);
+        if (!test) {
+            note += 'invalid Username! ';
+        }
+        test = reg.test(password);
+        if (!test) {
+            note += 'invalid Password! ';
+        }
+        setNotes(note);
+        return (note === '');
     }
 
     return (
@@ -37,6 +57,8 @@ function Login({ registered, setCurrentUser }) {
                                 <label htmlFor="password">Password</label>
                                 <input onKeyDown={handleKey} type="password" namename="password" placeholder="password" id="password" />
                             </div>
+                            <p><div className="notes"><p>{notes}</p></div></p>
+                            <p><div className="notes"><p>{userState}</p></div></p>
                             <button type="button" className="submit_button" onClick={handle}>Login</button>
                             <i className="button__icon fas fa-chevron-right"></i>
                         </form>
